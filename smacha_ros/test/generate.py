b358b1a93f6faa50afe1155e3f81d3f1d4f17e7d
Barry Ridge <barry.ridge@gmail.com>
2017-12-29 22:15:51 +0100

    Add another generate service test

--- smacha_ros/test/generate.py
+++ smacha_ros/test/generate.py
@@ -10,16 +10,18 @@ import re
 import difflib
 import yaml as pyyaml
 import stat
+
 import smacha
 from smacha_ros.srv import *
+from smacha.util import Tester
 
-class TestGenerate(unittest.TestCase):
+class TestServices(Tester):
     
     write_output_files = False
     debug_level = 1
     
     def __init__(self, *args, **kwargs):
-        super(TestGenerate, self).__init__(*args, **kwargs)
+        super(TestServices, self).__init__(*args, **kwargs)
 
         # Use the SMACHA parser for loading YAML scripts
         self._parser = smacha.Parser()
@@ -45,7 +47,7 @@ class TestGenerate(unittest.TestCase):
         # Name the script param
         script_param = 'smacha/scripts/' + os.path.splitext(os.path.basename(smacha_script_filename))[0]
 
-        # Load the script into the param server
+        # Load the script onto the param server
         try:
             rospy.set_param(script_param, script)
         except Exception as e:
@@ -70,45 +72,6 @@ class TestGenerate(unittest.TestCase):
 
         return smach_code
     
-    def _compare(self, code_a, code_b, file_a='code_a', file_b='code_b'):
-        """Diff compare code_a with code_b."""
-        # Strip single-line comments
-        code_a = re.sub('\s*#.*\n', '\n', code_a)
-        code_b = re.sub('\s*#.*\n', '\n', code_b)
-
-        # Squeeze whitespace after commas
-        code_a = re.sub(',\s+', ',', code_a)
-        code_b = re.sub(',\s+', ',', code_b)
-        
-        # Squeeze whitespace before and after colons
-        code_a = re.sub('\s*\:\s*', ':', code_a)
-        code_b = re.sub('\s*\:\s*', ':', code_b)
-
-        # Strip (python agnostic!) whitespace from both code strings and convert to lists
-        code_a_stripped = [line for line in code_a.strip().splitlines() if line != '' and
-                                                                           re.match('^\s+$', line) is None]
-        code_b_stripped = [line for line in code_b.strip().splitlines() if line != '' and
-                                                                           re.match('^\s+$', line) is None]
-        # Strip trailing whitespace from each line
-        code_a_stripped = [line.rstrip() for line in code_a_stripped]
-        code_b_stripped = [line.rstrip() for line in code_b_stripped]
-
-        if self.debug_level > 1:
-            print('\n' + file_a + ':\n')
-            print(code_a_stripped)
-            print('\n' + file_b + ':\n')
-            print(code_b_stripped)
-
-        # Use difflib to compare
-        same = True
-        for line in difflib.unified_diff(code_a_stripped, code_b_stripped, fromfile=file_a, tofile=file_b, lineterm=''):
-            if line:
-                if self.debug_level > 0:
-                    print(line)
-                same = False
-
-        return same
-
     def test_generate_hard_coded_params(self):
         """Test generating hard_coded_params.yml"""
         smacha_path = self._rospack.get_path('smacha')
@@ -116,9 +79,17 @@ class TestGenerate(unittest.TestCase):
             generated_code = self._generate(os.path.join(smacha_path, 'test/smacha_scripts/smacha_test_examples/hard_coded_params.yml'))
             original_code = original_file.read()
             self.assertTrue(self._compare(generated_code, original_code, file_a='generated', file_b='original'))
+    
+    def test_generate_assigned_params(self):
+        """Test generating assigned_params.yml"""
+        smacha_path = self._rospack.get_path('smacha')
+        with open(os.path.join(smacha_path, 'test/smacha_test_examples/params.py')) as original_file:
+            generated_code = self._generate(os.path.join(smacha_path, 'test/smacha_scripts/smacha_test_examples/assigned_params.yml'))
+            original_code = original_file.read()
+            self.assertTrue(self._compare(generated_code, original_code, file_a='generated', file_b='original'))
 
 
 if __name__=="__main__":
     
     rospy.init_node('test_smacha_ros_generate',log_level=rospy.DEBUG)
-    rostest.rosrun('smacha_ros', 'test_smacha_ros_generate', TestGenerate)
+    rostest.rosrun('smacha_ros', 'test_smacha_ros_generate', TestServices)
